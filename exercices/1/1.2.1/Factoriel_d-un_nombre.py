c28e70d1f3a35ce0d2d75409c66383446b3cbc1e
huet guillaume <gads31400@gmail.com>
2019-04-12 12:30:21 +0200

    exercice 1.2.1

---
+++ exercices/1/1.2.1/Factoriel_d-un_nombre.py
@@ -0,0 +1,9 @@
+def facto(n):
+    init = n
+    res = n
+    i = -1
+    while n > 1:
+        i=i+1
+        res = res * (n - 1)
+        n = n-1 
+    return print( "Nombre d'opérations effectué ",i,"\nLe factorielle de ",init," est :", res)
